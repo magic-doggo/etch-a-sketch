ac4e77a5703e867d79be1efa4a07d379c993b585
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-18 16:32:33 +0000

    Change color on mouseover for any grid item

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const container = document.querySelector(".container")
 for ( i = 0; i < 256 ; i++){
   let oneGrid = document.createElement("div")
   container.appendChild(oneGrid)
-  oneGrid.textContent = "TEST"
+  oneGrid.textContent = "T"
   oneGrid.classList.add("gridClass")
 }
+
+const gridItem = document.querySelectorAll(".gridClass");
+gridItem.forEach(ravioli => {
+  ravioli.addEventListener("mouseover", onHover);
+  function onHover(){
+    ravioli.style.backgroundColor = "orange"
+  }
+  
+  
+});
+  
+//learn arrow functions!!!! and understand the function above! https://www.queryselectorall.com/addeventlistener
