663373fe4439b87ebd3f492bc445e1b892165983
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-19 16:16:18 +0000

    Add button allowing users to set the number of squares per grid side

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector(".container")
-
+// let inputLength;
 for ( i = 0; i < 256 ; i++){
   let oneGrid = document.createElement("div")
   container.appendChild(oneGrid)
-  oneGrid.textContent = "T"
+  oneGrid.textContent = ""
   oneGrid.classList.add("gridClass")
 }
 
@@ -19,9 +19,31 @@ gridItem.forEach(ravioli => {
 //const array1 = ['a', 'b', 'c'];   correct syntax for forEach
 //array1.forEach((element) => console.log(element));
 //learn callbacks!
+//document.querySelectorAll('.classname').forEach(e => e.remove());
 
 const squareNumber = document.querySelector(".squareNumber")
 squareNumber.addEventListener("click", setGridLengthSquares)
 function setGridLengthSquares () {
   let inputLength = parseInt(prompt("Choose how many squares long is the side of the grid", "10"));
+  if (inputLength > 100){
+    inputLength = 100;
+  }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  for ( i = 0; i < inputLength**2 ; i++){
+    let oneGrid = document.createElement("div")
+    container.appendChild(oneGrid)
+    oneGrid.textContent = "";
+    oneGrid.classList.add("gridClass");
+    let widthPercent = 100 / inputLength;
+    oneGrid.style.width = widthPercent + "%";
+  }
+  const gridItem = document.querySelectorAll(".gridClass");
+  gridItem.forEach(ravioli => {
+    ravioli.addEventListener("mouseover", onHover);
+    function onHover(){
+      ravioli.style.backgroundColor = "orange"
+   }
+  });
 }
